0dda39cb626064a2d145a4f34a74653b843cf123
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-08 21:51:41 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+<h1>hello</h1>
+<footer>
+          This project was coded by 
+           <a href='/'> Maryna Holinka </a>
+           and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
+          target="_blank"
+          rel="noopener noreferrer">
+     open-soursed on GitHub
+        </a>
+        </footer>
     </div>
   );
 }

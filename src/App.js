9d579eded865b622f417763ebe836d5b6832f7a4
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-08 23:03:54 +0100

    sum

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-         <h1>hello</h1>
+        
          <Weather/>
           <footer>
           This project was coded by 
--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,37 @@ import './Weather.css';
  export default function Weather() {
   return (
     <div className='Weather'>
-    Weather
-</div>
-  )
+          <form>
+        <div class="row">
+          <div class="col-9">
+            <input type="search" placeholder="Search a city" class="form-control" autocomplete="off"/>
+          </div>
+          <div class="col-3">
+           <input type="submit" value="Search" class="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+        <h1>London</h1> 
+   <ul>
+    <li>Friday 20:23</li>
+    <li>Cloudy</li>
+   </ul>
+   <div class="row">
+  <div class="col-6">
+  <img src="https://openweathermap.org/img/wn/01n@2x.png" alt="clear"/>
+  18°
+    </div>
+    <div class="col-6">
+    <ul>
+        <li>
+            Humidity: 23%
+        </li>
+        <li>
+            Wind: 12 km/h
+        </li>
+    </ul>
+    </div>
+  </div>
+  </div>
+)
 }
-

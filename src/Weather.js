6834e240798b4953f385fc5e5746d813e837a84c
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-09 21:05:07 +0100

    sum

--- src/Weather.js
+++ src/Weather.js
@@ -5,8 +5,19 @@ import {
     UilTear,
     UilWind,
   } from '@iconscout/react-unicons';
+  import axios from 'axios';
 
  export default function Weather() {
+function handleResponse(response) {
+    console.log(response.data);
+}
+
+    const apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
+    let city = 'London';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
+    ${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse); 
+
   return (
     <div className='Weather'>
           <form>

c577a65b29e10cfb2b2c47d1885c3078c22771aa
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-11 23:29:23 +0100

    sum

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,37 +1,44 @@
-import React from 'react'
-import WeatherIcon from './WeatherIcon';
+import React, { useState } from 'react'
+
 import './WeatherForecast.css';
 import axios from 'axios';
+import WeatherForecastDay from './WeatherForecastDay';
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+let [forecast, setForecast] = useState(null);
+
 function handleResponse(response) {
-  console.log(response.data);
+  // console.log(response.data.list);
+  setForecast(response.data.list);
+  setLoaded(true);
+ 
 }
 // console.log(props)
-  let apiKey = 'df04a6426eb8c9305ebb65c9deb52f35';
+
+if (loaded) {
+
+ console.log(forecast);
+
+   return (
+   <div className='WeatherForecast'>
+<div className='row'>
+  <div className='col'>
+   <WeatherForecastDay data={forecast[0]}/>
+  </div>
+</div>
+    </div>
+    ); 
+ 
+} else {
+      let apiKey = 'df04a6426eb8c9305ebb65c9deb52f35';
   let longitude = props.coordinates.lon;
   let latitude = props.coordinates.lat;
 let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?
 lat=${latitude}&lon=${longitude}&cnt=5&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(handleResponse); 
 
-  return (
-    <div className='WeatherForecast'>
-<div className='row'>
-  <div className='col'>
-    <div className='WeatherForecast-day'>
-      Mon 
-      </div>
-    <WeatherIcon code ='01d' size={44}/> 
-    <div className='WeatherForecast-temperature'>
-      <span className='WeatherForecast-temperature-max'>20°</span>
-      <span className='WeatherForecast-temperature-min'>14°</span>
-      </div>
-  </div>
-</div>
-    </div>
-  );
-    
-  
+  return null; 
 }
+ }
 
